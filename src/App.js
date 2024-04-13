c670c23cef22134abe10069c91c4138baca97e7e
S-ChandanaSri <chandanaofficial8@gmail.com>
2024-04-13 15:59:38 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {  BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 
 import Weathe from './component/Weathe';
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Weathe />} />
+         
         </Routes>
       </Router>
 
--- src/component/Weathe.js
+++ src/component/Weathe.js
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+
 
 
 function Weathe() {
 
     const [filtered,setFiltered]=useState([]);
-    const [dis,setDis]=useState('');
+    const [dis,setDis]=useState('hyderabad');
     const [weather,setWeather]=useState(null)
 
 
 
 const onsea=(event)=>{
-    event.target.name=event.target.value;
+    event.target.name=event.target.value
     setDis(event.target.name)
     
 }
 
+
+
+const fetchWeatherData = (city) => {
+  const apiKey = "736777f2bf3b65e3d9d7047bc3ac07df";
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log("Weather data received:", data);
+      setWeather(data);
+      setFiltered([...filtered, city]);
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+    });
+};
+
+useEffect(() => {
+  fetchWeatherData(dis); 
+}, []);
+
 const onsearch = (e) => {
-    e.preventDefault();
-    const apiKey = "736777f2bf3b65e3d9d7047bc3ac07df";
-
-    
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${dis}&appid=${apiKey}`)
-
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); 
-      })
-      .then(data => {
-        console.log("Weather data received:", data);
-        setWeather(data);
-        setFiltered([...filtered, dis]);
-      })
-      .catch(error => {
-     
-        console.error('Error fetching weather data:', error);
-      });
-  };
+  e.preventDefault();
+  fetchWeatherData(dis);
+};
   
 
 
@@ -48,19 +54,19 @@ const onsearch = (e) => {
     <div>
     <div className='container'>
    
-        <form >
+        <form  >
                 <div className="search"   >
                     
                     
-                    <input  className="input-field"  placeholder="Enter Location"  onChange={onsea} />  
+                    <input  className="input-field"  placeholder="Enter Location"  onChange={onsea} value={dis}  />  
                     &nbsp;<span className="search-icon-container">
                     <i className="fa-solid fa-magnifying-glass search-icon"  onClick={onsearch}></i>
 
                     </span>
 
-                    <div className="recentsearch">
+                    <div className="recentsearch" >
                       {filtered.map((item, index) => (
-                         <div className="search-item" key={index}>
+                         <div className="search-item" key={index} >
                             <i className="fa-solid fa-magnifying-glass"></i>&nbsp;&nbsp;{item}
                           </div>
                          ))}
@@ -70,6 +76,7 @@ const onsearch = (e) => {
             </form>
 
           <img className="weather-image"src='https://i.pinimg.com/originals/9e/76/44/9e76440e228671d77b9afc06887fab3c.png'></img>
+          
                 
   {weather && (
     <div className="weather-info">
@@ -85,7 +92,7 @@ const onsearch = (e) => {
       <div>Wind Speed</div>
         </div>
         <div className="humidity">
-          <div style={{fontSize:"1.5rem"}} >{(weather.list[0].main.humidity).toFixed(0)}%</div>
+          <div style={{fontSize:"1.8rem"}} >{(weather.list[0].main.humidity).toFixed(0)}%</div>
           <div >Humidity</div>
         </div>
       
